156cb11a817fbe60882358ff496696ac4551c44a
Gleb Gorelov <g.gorelov@haulmont.com>
2023-09-05 13:56:57 +0400

    Day 3.2

--- src/main/java/com/company/jmixpm/screen/task/TaskBrowse.java
+++ src/main/java/com/company/jmixpm/screen/task/TaskBrowse.java
@@ -1,10 +1,20 @@
 package com.company.jmixpm.screen.task;
 
-import io.jmix.ui.screen.*;
 import com.company.jmixpm.entity.Task;
+import io.jmix.ui.screen.*;
 
 @UiController("Task_.browse")
 @UiDescriptor("task-browse.xml")
 @LookupComponent("tasksTable")
 public class TaskBrowse extends StandardLookup<Task> {
+
+    @Install(to = "tasksTable", subject = "styleProvider")
+    private String tasksTableStyleProvider(final Task entity, final String property) {
+        if (entity.getEstimatedEfforts() == null
+                && ("estimatedEfforts".equals(property) || property == null)) {
+            return "no-estimated-efforts";
+        }
+
+        return null;
+    }
 }
